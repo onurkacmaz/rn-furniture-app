5bec53a0c94d1a752a7a3c385c3d788ebcb0c7d5
Onur Kaçmaz <onur.kacmaz@path.com.tr>
2022-12-14 02:38:29 +0300

    edit catalog item

--- src/components/ItemComponent.js
+++ src/components/ItemComponent.js
@@ -20,17 +20,17 @@ const ItemComponent = (props) => {
 
   return (
     <TouchableOpacity style={styles.container} key={v.id} onPress={() => handleOpenItemDetail(v.id)}>
-      <View style={{flexDirection:'row'}}>
+      <View style={{flexDirection:'row', flex:1}}>
         <Text style={{flex:4, fontWeight:'bold', fontSize:20}}>{v.price}</Text>
         <TouchableOpacity onPress={() => setIsFavorite(!isFavorite)}>
           <Icon style={{flex:0}} name={isFavorite ? "heart" : "heart-o"} size={20} color="rgb(0,26,77)" />
         </TouchableOpacity>
       </View>
-      <View style={{marginTop:20, alignSelf:'center'}}>
+      <View style={{marginTop:20, alignSelf:'center', flex:1}}>
         <Image source={{uri: v.image, width:90, height:90}} width="100%" height="100%" />
       </View>
-      <Text style={{textAlign:'center', marginTop:10, fontSize:20}}>{v.name}</Text>
-      <View style={{padding:10, marginTop:10, backgroundColor:'#fff', borderRadius:10}}>
+      <Text style={{textAlign:'center', marginTop:10, fontSize:20, flex:1}}>{v.name}</Text>
+      <View style={{padding:10, backgroundColor:'#fff', borderRadius:10, flex:0, marginTop:10}}>
         <Text style={{textAlign:'center'}}>Rent for {v.additional.installment.value}</Text>
       </View>
     </TouchableOpacity>
@@ -41,6 +41,7 @@ export default ItemComponent
 
 const styles = StyleSheet.create({
   container: {
+    flex:1,
     padding:15,
     width:180,
     borderRadius:20,
