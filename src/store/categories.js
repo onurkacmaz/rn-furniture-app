8504335a1a83b411f276376be07db8c68e060754
Onur Kaçmaz <onur.kacmaz@path.com.tr>
2022-12-14 01:55:50 +0300

    init

---
+++ src/store/categories.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    name: "Home",
+    id: 1
+  },
+  {
+    name: "Office",
+    id: 2
+  },
+  {
+    name: "Terrace",
+    id: 3
+  },
+  {
+    name: "Club",
+    id: 4
+  },
+  {
+    name: "Desks",
+    id: 5
+  },
+  {
+    name: "Bathroom",
+    id: 6
+  }
+]
---
+++ src/store/products.js
@@ -0,0 +1,41 @@
+export default [
+  {
+    id: 1,
+    name: 'Florance Chair',
+    categoryId: 1,
+    image: 'https://purepng.com/public/uploads/large/purepng.com-armchairarmchairarmchairsoffice-chairssofa-1701527920404xw33e.png',
+    isFavorite: true,
+    price: '$897',
+    additional: {
+      installment: {
+        value: '$45/mo'
+      }
+    }
+  },
+  {
+    id: 2,
+    name: 'Hewitt Chair',
+    categoryId: 1,
+    image: 'https://snipstock.com/assets/cdn/png/cab2fcf3111bbb2ecba52a5032c8a5ed.png',
+    isFavorite: false,
+    price: '$120',
+    additional: {
+      installment: {
+        value: '$35/mo'
+      }
+    }
+  },
+  {
+    id: 3,
+    name: 'Harper Swivel Chair',
+    categoryId: 1,
+    image: 'https://snipstock.com/assets/cdn/png/fccfe3b4cc250341daafcb9a4079cc87.png',
+    isFavorite: false,
+    price: '$500',
+    additional: {
+      installment: {
+        value: '$28/mo'
+      }
+    }
+  }
+]
